dc3c14109fbc42e4a100e3832041a7d21912e8e6
Cristhian <cristhiangomezdev@gmail.com>
2022-10-21 21:02:49 +0200

    ejercicio omelette

---
+++ main.js
@@ -0,0 +1,14 @@
+let cheeseCalories = 200
+let eggsRequired = 4
+let caloriesperEgg = 75
+
+let eggsCalories = caloriesperEgg*4
+
+console.log("Las calorias de todos los huevos es:", eggsCalories)
+
+
+let omeletteDuFromageCalories = cheeseCalories + eggsCalories
+
+console.log("Las calorias del Omelette son:", omeletteDuFromageCalories)
+
+console.log("El valor de calorias por porción es:", omeletteDuFromageCalories/2)
